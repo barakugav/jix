3a6ee950f582441923e433eeb085788368954b8b
Barak Ugav <barakugav@gmail.com>
2026-06-20 11:34:44 +0300

    Add stubs for py func aliases (pow, abs, ...)

--- jix-py/src/bin/generate_pyi.rs
+++ jix-py/src/bin/generate_pyi.rs
@@ -1,6 +1,27 @@
+use std::fs;
+
 use pyo3_stub_gen::Result;
 
 fn main() -> Result<()> {
-    jix::__private::generate_pyi()?.generate()?;
+    let stub_info = jix::__private::generate_pyi()?;
+    stub_info.generate()?;
+    let pyi_path = stub_info.python_root.join("jix").join("__init__.pyi");
+    let mut pyi = fs::read_to_string(&pyi_path)?;
+
+    // Add docs for function aliases
+    pyi.push_str(
+        r#"
+def pow(a: typing.Any, b: typing.Any) -> Array:
+    """Alias for [`jix.power`][jix.power]"""
+
+def abs(array: typing.Any) -> Array:
+    """Alias for [`jix.absolute`][jix.absolute]"""
+
+def concat(arrays: typing.Sequence[typing.Any], axis: builtins.int = 0) -> Array:
+    """Alias for [`jix.concatenate`][jix.concatenate]"""
+"#,
+    );
+
+    fs::write(&pyi_path, pyi)?;
     Ok(())
 }
